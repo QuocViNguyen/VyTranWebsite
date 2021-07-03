318b367dd7661c9ad99edcf78ff49deb9e605e6b
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-07-03 18:32:38 -0500

    Added name

--- src/WelcomePage.js
+++ src/WelcomePage.js
@@ -1,31 +1,36 @@
 import React from 'react';
 import { Component } from 'react';
+import './WelcomePage.css';
 
 class WelcomePage extends Component {
     constructor(props) {
         super(props)
         this.addValue = 3
         this.state = { count: 1 }
+        this.name = 'VY TRAN'
     }
 
-    increaseCountBy5= () => {
-
+    increaseCountBy5(){
         this.setState((prevState, props) => ({
-                count: prevState.count + this.addValue
-            }))
-            console.log(this.state.count)
+        count: prevState.count + this.addValue
+        }))
+        // console.log(this.state.count)
     }
+
+    nameTitle = () => {
+        return <div className="nameTitle">{this.name}</div>
+    }
+    
 
     render() { 
         return (
             <div className="WelcomePage">
-                <button onClick={this.increaseCountBy5}>Click Me</button>
+                <button onClick={() => this.increaseCountBy5()}>Click Me</button>
                 <p>{this.state.count}</p>
+                {this.nameTitle()}
             </div>
           );
     }
 }
-
-
  
 export default WelcomePage;
