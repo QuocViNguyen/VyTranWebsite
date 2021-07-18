ee997e7985c378e7b13bc88d2574ed262c5535ab
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-07-18 00:19:25 -0500

    Render skill list dynamically

--- src/components/pages/SkillPage.js
+++ src/components/pages/SkillPage.js
@@ -10,12 +10,19 @@ import { CaretRightFill } from 'react-bootstrap-icons';
 class SkillPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            TechSkills : ['RStudio', 'SAS', 'Microsoft Office', 'Data Visualization', 'Mac OS/Windows OS'],
+            SoftSkills : ['Excellent interpersonal skills', 'Strong multi-tasker', 'Skilled to work in high pressure environments', 'Strong communicator']
+          }
     }
 
     li_styling = {
         border: 'none',
         backgroundColor: 'transparent'
+    }
+
+    ligr_styling = {
+        textAlign: 'left'
     }
 
     render() { 
@@ -25,12 +32,15 @@ class SkillPage extends Component {
                 <Row>
                     <Col sm={{ span:5 , offset: 1}}>
                     
-                        <ListGroup>
-                            <ListGroup.Item style={this.li_styling}><CaretRightFill /> Cras justo odio</ListGroup.Item>
+                        <ListGroup style={this.ligr_styling}>
+                            {/* <ListGroup.Item style={this.li_styling}><CaretRightFill /> Cras justo odio</ListGroup.Item>
                             <ListGroup.Item style={this.li_styling}><CaretRightFill /> Dapibus ac facilisis in</ListGroup.Item>
                             <ListGroup.Item style={this.li_styling}><CaretRightFill /> Morbi leo risus</ListGroup.Item>
                             <ListGroup.Item style={this.li_styling}><CaretRightFill /> Porta ac consectetur ac</ListGroup.Item>
-                            <ListGroup.Item style={this.li_styling}><CaretRightFill /> Vestibulum at eros</ListGroup.Item>
+                            <ListGroup.Item style={this.li_styling}><CaretRightFill /> Vestibulum at eros</ListGroup.Item> */}
+                            {this.state.TechSkills.map (SoftSkillFnc => (
+                                <ListGroup.Item style={this.li_styling}><CaretRightFill/>{SoftSkillFnc}</ListGroup.Item>
+                            ))}
                         </ListGroup>
                         
                     </Col>
